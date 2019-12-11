99fa1d276cdd4e2d053424cd6bdca044ad12d45d
Mr. Fantastic <paradox7jcr@gmail.com>
2019-12-11 13:48:59 -0600

    Initial Commit

---
+++ Paycompute/Controllers/EmployeeController.cs
@@ -0,0 +1,224 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Paycompute.Entity;
+using Paycompute.Models;
+using Paycompute.Services;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Paycompute.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private readonly IEmployeeService _employeeService;
+        private readonly IWebHostEnvironment _hostingEnvironment;
+
+        public EmployeeController(IEmployeeService employeeService, IWebHostEnvironment hostingEnvironment)
+        {
+            _employeeService = employeeService;
+            _hostingEnvironment = hostingEnvironment;
+        }
+
+        public IActionResult Index()
+        {
+            var employees = _employeeService.GetAll().Select(employee => new EmployeeIndexViewModel{
+                Id = employee.Id,
+                EmployeeNo = employee.EmployeeNo,
+                ImageUrl = employee.ImageUrl,
+                FullName = employee.FullName,
+                Gender = employee.Gender,
+                DateJoined = employee.DateJoined,
+                Designation = employee.Designation,
+                City = employee.City,
+            }).ToList();
+            return View(employees);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken] //Prevents XSS Forgery Attacks
+        public async Task<IActionResult> Create(EmployeeCreateViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var employee = new Employee
+                {
+                    Id = model.Id,
+                    EmployeeNo = model.EmployeeNo,
+                    FirstName = model.FirstName,
+                    MiddleName = model.MiddleName,
+                    LastName = model.LastName,
+                    FullName = model.FullName,
+                    Gender = model.Gender,
+                    Email = model.Email,
+                    DOB = model.DOB,
+                    DateJoined = model.DateJoined,
+                    Phone = model.Phone,
+                    NationalInsuranceNumber = model.NationalInsuranceNumber,
+                    PaymentMethod = model.PaymentMethod,
+                    StudentLoan = model.StudentLoan,
+                    UnionMember = model.UnionMember,
+                    Address = model.Address,
+                    City = model.City,
+                    Postcode = model.Postcode,
+                    Designation = model.Designation,
+                };
+
+                if(model.ImageUrl != null && model.ImageUrl.Length > 0)
+                {
+                    var uploadDir = @"images/employee";
+                    var fileName = Path.GetFileNameWithoutExtension(model.ImageUrl.FileName);
+                    var extension = Path.GetExtension(model.ImageUrl.FileName);
+                    var webRootPath = _hostingEnvironment.WebRootPath;
+                    fileName = DateTime.UtcNow.ToString("yymmssfff") + fileName + extension;
+                    var path = Path.Combine(webRootPath, uploadDir, fileName);
+                    await model.ImageUrl.CopyToAsync(new FileStream(path, FileMode.Create));
+                    employee.ImageUrl = "/" + uploadDir + "/" + fileName;
+                }
+                await _employeeService.CreateAsync(employee);
+                return RedirectToAction(nameof(Index));
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            var employee = _employeeService.GetById(id);
+            if (employee == null)
+                return NotFound();
+
+            var model = new EmployeeEditViewModel()
+            {
+                Id = employee.Id,
+                EmployeeNo = employee.EmployeeNo,
+                FirstName = employee.FirstName,
+                MiddleName = employee.MiddleName,
+                LastName = employee.LastName,
+                Gender = employee.Gender,
+                Email = employee.Email,
+                DOB = employee.DOB,
+                DateJoined = employee.DateJoined,
+                Phone = employee.Phone,
+                NationalInsuranceNumber = employee.NationalInsuranceNumber,
+                PaymentMethod = employee.PaymentMethod,
+                StudentLoan = employee.StudentLoan,
+                UnionMember = employee.UnionMember,
+                Address = employee.Address,
+                City = employee.City,
+                Postcode = employee.Postcode,
+                Designation = employee.Designation,
+            };
+            CreateImageUrlIfExists(employee, model.ImageUrl);
+            return View(model);
+        }
+        
+        [HttpPost]
+        public async Task<IActionResult> Edit(EmployeeEditViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var employee = _employeeService.GetById(model.Id);
+                if (employee == null)
+                    return NotFound();
+
+                employee.EmployeeNo = model.EmployeeNo;
+                employee.FirstName = model.FirstName;
+                employee.LastName = model.LastName;
+                employee.MiddleName = model.MiddleName;
+                employee.NationalInsuranceNumber = model.NationalInsuranceNumber;
+                employee.Gender = model.Gender;
+                employee.Email = model.Email;
+                employee.DOB = model.DOB;
+                employee.DateJoined = model.DateJoined;
+                employee.Phone = model.Phone;
+                employee.Designation = model.Designation;
+                employee.PaymentMethod = model.PaymentMethod;
+                employee.StudentLoan = model.StudentLoan;
+                employee.UnionMember = model.UnionMember;
+                employee.Address = model.Address;
+                employee.City = model.City;
+                employee.Postcode = model.Postcode;
+                CreateImageUrlIfExists(employee, model.ImageUrl);
+                await _employeeService.UpdateAsync(employee);
+                return RedirectToAction(nameof(Index));
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Detail(int id)
+        {
+            var employee = _employeeService.GetById(id);
+            if (employee == null)
+                return NotFound();
+
+            var model = new EmployeeDetailViewModel()
+            {
+                Id = employee.Id,
+                EmployeeNo = employee.EmployeeNo,
+                FullName = employee.FullName,
+                Gender = employee.Gender,
+                DOB = employee.DOB,
+                DateJoined = employee.DateJoined,
+                Designation = employee.Designation,
+                NationalInsuranceNumber = employee.NationalInsuranceNumber,
+                Phone = employee.Phone,
+                Email = employee.Email,
+                PaymentMethod = employee.PaymentMethod,
+                StudentLoan = employee.StudentLoan,
+                UnionMember = employee.UnionMember,
+                Address = employee.Address,
+                City = employee.City,
+                ImageUrl = employee.ImageUrl,
+                Postcode = employee.Postcode,
+
+            };
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            var employee = _employeeService.GetById(id);
+            if (employee == null)
+                return NotFound();
+            var model = new EmployeeDeleteViewModel()
+            {
+                Id = employee.Id,
+                FullName = employee.FullName,
+            };
+            return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Delete(EmployeeDeleteViewModel model)
+        {
+            await _employeeService.Delete(model.Id);
+            return RedirectToAction(nameof(Index));
+        }
+
+        #region [Private Helpers]
+
+        private async void CreateImageUrlIfExists(Employee employee, IFormFile image)
+        {
+            if(image != null && image.Length > 0)
+            {
+                var uploadDir = @"images/employee";
+                var fileName = Path.GetFileNameWithoutExtension(image.FileName);
+                var extension = Path.GetExtension(image.FileName);
+                var webRootPath = _hostingEnvironment.WebRootPath;
+                fileName = DateTime.UtcNow.ToString("yymmssfff") + fileName + extension;
+                var path = Path.Combine(webRootPath, uploadDir, fileName);
+                await image.CopyToAsync(new FileStream(path, FileMode.Create));
+                employee.ImageUrl = "/" + uploadDir + "/" + fileName;
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ Paycomputer.Persistence/Migrations/20191210194607_UpdatedEmployee.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Paycomputer.Persistence.Migrations
+{
+    public partial class UpdatedEmployee : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EmployeeNumber",
+                table: "Employees");
+
+            migrationBuilder.AddColumn<string>(
+                name: "EmployeeNo",
+                table: "Employees",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Phone",
+                table: "Employees",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EmployeeNo",
+                table: "Employees");
+
+            migrationBuilder.DropColumn(
+                name: "Phone",
+                table: "Employees");
+
+            migrationBuilder.AddColumn<string>(
+                name: "EmployeeNumber",
+                table: "Employees",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
