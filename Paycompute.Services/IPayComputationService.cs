da96d51584626f1d8316290606c26da8ceaeeced
Mr. Fantastic <paradox7jcr@gmail.com>
2019-12-16 14:51:01 -0600

    PayComputationServices

---
+++ Paycompute.Services/IPayComputationService.cs
@@ -0,0 +1,24 @@
+﻿using Paycompute.Entity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace Paycompute.Services
+{
+    public interface IPayComputationService
+    {
+        Task CreateAsync(PaymentRecord paymentRecord);
+        PaymentRecord GetById(int id);
+        IEnumerable<PaymentRecord> GetAll();
+        IEnumerable<SelectListItem> GetAllTaxYear();
+        decimal OvertimeHours(decimal hoursWorked, decimal contractualHours);
+        decimal ContractualEarnings(decimal contractualHours, decimal hoursWorked, decimal hourlyRate);
+        decimal OvertimeRate(decimal hourlyRate);
+        decimal OvertimeEarnings(decimal overtimeRate, decimal overtimeHours);
+        decimal TotalEarnings(decimal overtimeEarnings, decimal contractualEarnings);
+        decimal TotalDeduction(decimal tax, decimal nationalInsuranceContribution, decimal studentLoanRepayment, decimal unionFees);
+        decimal NetPay(decimal totalEarnings, decimal totalDeduction);
+    }
+}
